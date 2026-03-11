b028c8e7f371895b38abbca59390fb8cf5009e46
lauraroballo <lauradaniela022006@gmail.com>
2026-03-10 21:38:57 -0500

    Ciclos, iteractivas y excepciones

---
+++ Condicionales/src/condicionales2.java
@@ -0,0 +1,15 @@
+/**
+ * Escribir un programa que pida al usuario dos
+ * números y muestre por pantalla su división.
+ * Si el divisor es cero el programa debe mostrar un error.
+ */
+import java.util.Scanner;
+public class condicionales2 {
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Escriba un número");
+        int numberOne = sc.nextInt();
+
+    }
+}
