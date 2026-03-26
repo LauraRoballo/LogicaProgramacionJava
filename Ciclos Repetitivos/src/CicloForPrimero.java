cec1db314523789cd7f6ef8e0c38d3e5e2ae8955
lauraroballo <lauradaniela022006@gmail.com>
2026-03-26 08:32:08 -0500

    Ciclos repetitivos

---
+++ Ciclos Repetitivos/src/CicloForPrimero.java
@@ -0,0 +1,10 @@
+
+public class CicloForPrimero {
+    public static void main(String[] args) {
+        System.out.println("--Numeros del 1 al 10--");
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
